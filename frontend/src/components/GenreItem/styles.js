763d09651746e3b8971f1acbead1997841a14b8f
Alla Rakhimova <alla.rakhimova103@gmail.com>
2022-02-20 22:23:52 +0200

    UI fixes

--- frontend/src/components/GenreItem/styles.js
+++ frontend/src/components/GenreItem/styles.js
@@ -19,6 +19,16 @@ export const GenreItemWrapper = styled.button`
     color: ${(props) => props.theme.colors.light};
   }
 
+  &.active {
+    background-color: ${(props) => props.theme.colors.secondary};
+    color: ${(props) => props.theme.colors.light};
+  }
+
+  &.active:hover {
+    background-color: transparent;
+    color: ${(props) => props.theme.colors.secondary};
+  }
+
   @media only screen and (max-width: 768px) {
     font-size: 12px;
     line-height: 15px;
