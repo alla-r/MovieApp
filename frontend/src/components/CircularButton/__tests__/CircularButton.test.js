2dbeb0556d0237c8b5063dd92305f224528357f0
Alla Rakhimova <alla.rakhimova103@gmail.com>
2022-03-20 22:16:56 +0200

    CircularButton tests

---
+++ frontend/src/components/CircularButton/__tests__/CircularButton.test.js
@@ -0,0 +1,29 @@
+import { shallow } from 'enzyme';
+import CircularButton from '../CircularButton';
+import HeartIcon from '../../../global/images/heart-icon.svg';
+
+describe('<CircularButton />', () => {
+  let defaultProps;
+
+  beforeEach(() => {
+    defaultProps = {
+      onClickHandler: () => {},
+      isActive: false,
+      iconSrc: HeartIcon
+    }
+  });
+  
+  it('should be defined', () => {
+    const wrapper = shallow(<CircularButton {...defaultProps} />)
+
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should have active class', () => {
+    defaultProps.isActive = true;
+    const wrapper = shallow(<CircularButton {...defaultProps} />)
+
+    expect(wrapper.find('.active')).toBeTruthy();
+  });
+});
+
