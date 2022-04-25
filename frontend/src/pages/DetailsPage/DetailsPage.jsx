13a1177eb0fe2c08bff8fec9900f1750bf7c8904
Alla Rakhimova <alla.rakhimova103@gmail.com>
2022-04-25 17:55:24 +0300

    add state handlers for circular buttons on Details page

--- frontend/src/pages/DetailsPage/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage/DetailsPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-param-reassign */
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../../components/Loader';
@@ -16,6 +17,9 @@ const DetailsPage = () => {
   const dispatch = useDispatch();
   const params = useParams();
 
+  const [isInFavorite, setIsInFavorite] = useState(false);
+  const [isInWatchList, setIsInWatchlist] = useState(false);
+
   const detailsLoading = useSelector(selectors.detailsLoading);
   const detailsData = useSelector(selectors.detailsData);
   const detailsCastList = useSelector(selectors.detailsCastList);
@@ -23,7 +27,19 @@ const DetailsPage = () => {
   const detailsRecommendationsList = useSelector(selectors.detailsRecommendationsList);
   const detailsError = useSelector(selectors.detailsError);
 
-  // console.log(detailsLoading);
+  const favoriteCallback = () => setIsInFavorite(!isInFavorite);
+  const watchlistCallback = () => setIsInWatchlist(!isInWatchList);
+
+  constants.CIRCULAR_BUTTONS_CONFIG?.forEach((btnConfig) => {
+    if (btnConfig.id === "favorite") {
+      btnConfig.onClickHandler = favoriteCallback;
+      btnConfig.isActive = isInFavorite;
+    }
+    if (btnConfig.id === "watchlist") {
+      btnConfig.onClickHandler = watchlistCallback;
+      btnConfig.isActive = isInWatchList;
+    }
+  });
 
   useEffect(() => {
     dispatch(actions.getMediaDetails(params.type, params.id));
