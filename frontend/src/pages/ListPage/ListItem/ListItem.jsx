4830c24723511d2902aa487d5e9b749d55f3e6f3
Alla Rakhimova <alla.rakhimova103@gmail.com>
2022-05-09 21:58:51 +0300

    prettier

--- frontend/src/pages/ListPage/ListItem/ListItem.jsx
+++ frontend/src/pages/ListPage/ListItem/ListItem.jsx
@@ -4,30 +4,30 @@ import PropTypes from 'prop-types';
 import { ItemContainer, CloseButton } from './styles';
 import { getFormattedDate } from '../../../global/helpers';
 
-
-const ListItem = ({details}) => {
+const ListItem = ({ details }) => {
   const navigate = useNavigate();
   const navigateToDetails = () => navigate(`/${details.type}/${details.id}`);
 
   return (
     <ItemContainer onClick={navigateToDetails}>
-        <img src={details.poster} alt="poster" className="poster" />
+      <img src={details.poster} alt="poster" className="poster" />
 
-        <div className="content-container">
-          <div className="content">
-            <h6 className="title">{details.title}</h6>
-            <p className="date">{getFormattedDate(details.date || details.firstAirDate)}</p>
-            <p className="description">{details.overview}</p>
-          </div> 
-          <div>
-          <CloseButton 
-          onClick={(e) => {
-            e.stopPropagation();
-            console.log('close');
-            console.log(details.id);
-            }} />
-            </div>
+      <div className="content-container">
+        <div className="content">
+          <h6 className="title">{details.title}</h6>
+          <p className="date">{getFormattedDate(details.date || details.firstAirDate)}</p>
+          <p className="description">{details.overview}</p>
         </div>
+        <div>
+          <CloseButton
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log('close');
+              console.log(details.id);
+            }}
+          />
+        </div>
+      </div>
     </ItemContainer>
   );
 };
