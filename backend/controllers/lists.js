d31a0240fb41c61f6faa4fb892768180e8e50c2b
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-01-26 10:26:24 +0200

    replace .then with async/await

--- backend/controllers/lists.js
+++ backend/controllers/lists.js
@@ -22,103 +22,108 @@ const getTokenFrom = request => {
   return null
 }
 
-listRouter.get('/:listName', async (request, response) => {
-  const list = request.params.listName;
+listRouter.get('/:listName', async (request, response, next) => {
+  try {
+    const list = request.params.listName;
 
-  const userId = "63c3157381900870daba5d9a"
-  const user = await User.findById(userId)
+    const userId = "63c3157381900870daba5d9a"
+    const user = await User.findById(userId)
 
-  const notes = await LIST_MODELS[list].find({}).populate("user");
-  // console.log(notes)
+    const notes = await LIST_MODELS[list].find({}).populate("user");
 
-  const newNotes = notes.filter((note) => {
-    console.log(note.userId)
-    console.log(userId)
-    return note.userId === userId
-  });
+    const newNotes = notes.filter((note) => {
+      console.log(note.userId)
+      console.log(userId)
+      return note.userId === userId
+    });
 
-  console.log(newNotes)
+    console.log(newNotes)
 
-  response.json(newNotes);
-})
-
-listRouter.get('/:listName/:id', (request, response, next) => {
-  const list = request.params.listName;
-
-  LIST_MODELS[list].findById(request.params.id)
-    .then(item => {
-      if (item) {
-        response.json(item);
-      } else {
-        response.status(404).end()
-      }
-      
-    })
-    .catch(error => {
-      next(error)
-    })
-})
-
-listRouter.post('/:listName', async (request, response) => {
-  const body = request.body;
-  const list = request.params.listName;
-  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+    response.json(newNotes);
+  } catch(error) {
+    next(error)
   }
+})
 
-  if (!body.details) {
-    return response.status(400).json({
-      error: 'content is missing'
-    })
+listRouter.get('/:listName/:id', async (request, response, next) => {
+  try {
+    const list = request.params.listName;
+
+    const item = await LIST_MODELS[list].findById(request.params.id)
+
+    if (item) {
+      response.json(item);
+    } else {
+      response.status(404).end()
+    }
+  } catch(error) {
+    next(error)
   }
-
-  const user = await User.findById(decodedToken.id)
-
-  const item = new LIST_MODELS[list]({
-    id: body.details.id,
-    user: user,
-    userId: user._id,
-    type: body.details.type,
-    poster: body.details.poster,
-    title: body.details.title,
-    overview: body.details.overview,
-    firstAirDate: body.details.firstAirDate,
-    date: body.details.date,
-    timestamp: body.details.timestamp,
-    rate: body.details.rate,
-  })
-
-  const savedItem = await item.save()
-  response.json(savedItem)
 })
 
-listRouter.delete('/:listName/:id', (request, response, next) => {
-  const list = request.params.listName;
+listRouter.post('/:listName', async (request, response, next) => {
+  try {
+    const body = request.body;
+    const list = request.params.listName;
+    const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
 
-  LIST_MODELS[list].findByIdAndRemove(request.params.id)
-    .then(item => {
-      response.status(204).end();
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    if (!body.details) {
+      return response.status(400).json({
+        error: 'content is missing'
+      })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const item = new LIST_MODELS[list]({
+      id: body.details.id,
+      user: user,
+      userId: user._id,
+      type: body.details.type,
+      poster: body.details.poster,
+      title: body.details.title,
+      overview: body.details.overview,
+      firstAirDate: body.details.firstAirDate,
+      date: body.details.date,
+      timestamp: body.details.timestamp,
+      rate: body.details.rate,
     })
-    .catch(error => {
-      next(error)
-    })
+
+    const savedItem = await item.save()
+    response.json(savedItem)
+  } catch(error) {
+    next(error)
+  }
 })
 
-listRouter.put('/:listName/:id', (request, response, next) => {
-  const body = request.body
-  const list = request.params.listName;
-  const { rate } = body;
+listRouter.delete('/:listName/:id', async (request, response, next) => {
+  try {
+    const list = request.params.listName;
+    const removedItem = await LIST_MODELS[list].findByIdAndRemove(request.params.id)
 
-  LIST_MODELS[list].findByIdAndUpdate(request.params.id, { rate }, { new: true })
-    .then(updatedItem => {
-      console.log(updatedItem)
-      response.json(updatedItem);
-    })
-    .catch(error => {
-      next(error)
-    })
+    response.status(204).end();
+  } catch(error) {
+    next(error)
+  }
+})
+
+listRouter.put('/:listName/:id', async (request, response, next) => {
+  try {
+    const body = request.body
+    const list = request.params.listName;
+    const { rate } = body;
+
+    const updatedItem = await LIST_MODELS[list].findByIdAndUpdate(request.params.id, { rate }, { new: true })
+    console.log(updatedItem)
+
+    response.json(updatedItem);
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = listRouter
