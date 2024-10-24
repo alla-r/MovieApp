c365225ad3a234a0a9c6eb18a4ef937846619264
Alla Rakhimova <alla.rakhimova103@gmail.com>
2024-10-24 17:25:06 +0300

    add Logo.test

---
+++ frontend/src/components/Logo/Logo.test.jsx
@@ -0,0 +1,31 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '../../utils/helpers/test-utils';
+import Logo from './Logo';
+
+describe('<Logo />', () => {
+  let container;
+  const mockHandler = vi.fn();
+
+  beforeEach(() => {
+    container = render(<Logo onClickHandler={mockHandler} />).container;
+  });
+
+  it('should be defined', () => {
+    expect(container).toBeDefined();
+  });
+
+  it('should have MovieAPP text content', () => {
+    expect(container).toHaveTextContent('MovieAPP');
+  });
+
+  it('should call onClickHandler after clicking on logo', async () => {
+    // screen.debug(container);
+    const user = userEvent.setup();
+    const element = screen.getByTestId('logo-container');
+    await user.click(element);
+
+    console.log(mockHandler.mock);
+
+    expect(mockHandler.mock.calls).toHaveLength(1);
+  });
+});
